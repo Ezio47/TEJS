79e5c3c22bf8bd2565864f03fbc257065a50c802
Kyle Burrows <kylbrrws@gmail.com>
2016-01-07 23:01:46 -0500

    Added all members and functions to IProjectTree, added some intellisense as well

--- TEJS.intellisense.js
+++ TEJS.intellisense.js
@@ -12,43 +12,43 @@
     return {
 
         /// <field type="IAnalysis">Methods for analyzing the terrain</field>
-        Analysis: {}
+        Analysis: new IAnalysis()
 
         /// <field type="">Controls various application level settings</field>
-        ,Application:{}
+        ,Application: new IApplication()
 
         /// <field type="ICommand">Gives access to standard TerraExplorer menu commands</field>
-        , Command: {}
+        , Command: new ICommand()
 
         /// <field type="ICoordServices">Provides a set of methods for reprojecting given coordinates between different coordinate systems</field>
-        , CoordServices: {}
+        , CoordServices: new ICoordServices()
 
         /// <field type="ICreator">Composed of many methods that create different objects</field>
-        , Creator: {}
+        , Creator: new ICreator()
 
         /// <field type="IDateTime">Provides you with date and time related features</field>
-        , DateTime: {}
+        , DateTime: new IDateTime()
 
         /// <field type="Boolean">Gets and sets a Boolean that determines whether the containing application executes all keyboard acceleration commands or TerraExplorer executes them</field>
         , IgnoreAccelerators: false
 
         /// <field type="INavigate">Gives you control of the camera in terms of positioning, orientation, and speed</field>
-        , Navigate: {}
+        , Navigate: new INavigate()
 
         /// <field type="IProject">Gives access to project settings and to basic methods for opening and saving the project</field>
-        , Project: {}
+        , Project: new IProject()
 
         /// <field type="IProjectTree">Allows you to manipulate the Project Tree items, including management of groups, items and layers</field>
         , ProjectTree: new IProjectTree()
 
         /// <field type="ITerrain">An ITerrain object representing information about the terrain, such as the MPT file name that is being used, and information about the MPT coordinate system</field>
-        , Terrain: {}
+        , Terrain: new ITerrain()
 
         /// <field type="IVersionInfo">Gets information about the version of TerraExplorer that is running</field>
-        , Version: {}
+        , Version: new IVersionInfo()
 
         /// <field type="IWindow">A wrapper for all the functionality that represents interaction with the 3D Window, such as snapshots, dimensions and query pixel information.</field>
-        ,Window:{}
+        ,Window: new IWindow()
  
 
         ,AttachEvent: function(){
@@ -193,18 +193,274 @@
 */
     }; // End Return
 
-
+    function IAnalysis()
+    {
+        return {};
+    }
+    function IApplication()
+    {
+        return {};
+    }
+    function ICommand() {
+        return {};
+    }
+    function ICoordServices() {
+        return {};
+    }
+    function ICreator() {
+        return {};
+    }
+    function IDateTime() {
+        return {};
+    }
+    function INavigate() {
+        return {};
+    }
+    function IProject() {
+        return {};
+    }
     function IProjectTree() {
         return {
-            GetObject: function (ID) {
-                /// <summary>Returns an interface to an object based on its ID. If IProjectTree.GetObject fails, it might be that the item that you are trying to access is actually a group (which has no interface).
-                /// <para>To determine whether a Project Tree element is a group, use IProjectTree.IsGroup. </para>
-                /// </summary>
-                /// <param name="ID" type="String">The requested item ID.</param>
-                /// <returns type="ITerraExplorerObject" />
-            }
+            //Members
+
+            /// <field type="String">Gets the ID of the hidden group tree element. When this ID is passed as an element’s ParentGroupID, the element will be added under a hidden group</field>
+            HiddenGroupID:'',
+            /// <field type="String">Gets the name of the hidden group tree element. When this name is passed as an element’s ParentGroupName, the element will be added under a hidden group</field>
+ 	        HiddenGroupName:'',
+            /// <field type="String">Gets the ID that should be passed as an element’s ParentGroupID, if you do not want the element added to the Project Tree</field>
+ 	        NotInTreeID:'',
+            /// <field type="String">Gets the ID of the Tree root element. When this ID is passed as an element’s ParentGroupID, the element will be added under the Tree root
+            /// <para>Note:	In some methods (e.g. all CreateXXX methods such as Create3DArrow), an element can also be created under the Tree root by passing an empty string for the ParentGroupID</para>
+            /// </field>
+ 	        RootID:'',
+            
+            // Methods
+
+ 	        CreateGroup: function (GroupName, ParentGroupID) {
+ 	            /// <signature>
+ 	            /// <summary>This method creates a group in root of the Project Tree
+ 	            /// <para>Returns the ID of the group being created</para></summary>
+ 	            /// <param name="GroupName" type="String">Name assigned to the group being created</param>
+ 	            /// <returns type="String" />
+ 	            /// </signature>
+ 	            /// <signature>
+ 	            /// <summary>This method creates a group in the Project Tree
+ 	            /// <para>Returns the ID of the group being created</para></summary>
+ 	            /// <param name="GroupName" type="String">Name assigned to the group being created</param>
+ 	            /// <param name="ParentGroupID" type="String">The Project Tree group in which the group is created. If it is set to an empty string, the object is created under the root</param>
+ 	            /// <returns type="String" />
+ 	            /// </signature>
+ 	        },
+ 	        CreateLockedGroup: function(GroupName, ParentGroupID) {
+ 	            /// <signature>
+ 	            /// <summary>Creates a group in a “locked” (collapsed) mode. When locked, TerraExplorer Viewer users cannot expand the group to view its contents
+ 	            /// <para>Returns the ID of the group being created</para></summary>
+ 	            /// <param name="GroupName" type="String">Name assigned to the group being created</param>
+ 	            /// <returns type="String" />
+ 	            /// </signature>
+ 	            /// <signature>
+ 	            /// <summary>Creates a group in a “locked” (collapsed) mode. When locked, TerraExplorer Viewer users cannot expand the group to view its contents
+ 	            /// <para>Returns the ID of the group being created</para></summary>
+ 	            /// <param name="GroupName" type="String">Name assigned to the group being created</param>
+ 	            /// <param name="ParentGroupID" type="String">The Project Tree group in which the group is created. If it is set to an empty string, the object is created under the root</param>
+ 	            /// <returns type="String" />
+ 	            /// </signature>
+ 	        },
+ 	        DeleteItem: function (ID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        EditItem: function (ID, Flags) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        EditItems: function (Items) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        EnableRedraw: function (bEnable) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        EndEdit: function() {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        ExpandGroup: function (GroupID, bExpand) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        FindItem: function (PathName) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        GetActivationCode: function (GroupID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        GetClientData: function (ID, Namespace) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        GetGroupEndTime: function (GroupID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        GetGroupLocation: function (GroupID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        GetGroupStartTime: function (GroupID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        GetItemName: function(ID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        GetLayer: function (GroupID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        GetNextItem: function (ID,Code) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        GetObject: function (ID) {
+ 	            /// <summary>Returns an interface to an object based on its ID. If IProjectTree.GetObject fails, it might be that the item that you are trying to access is actually a group (which has no interface).
+ 	            /// <para>To determine whether a Project Tree element is a group, use IProjectTree.IsGroup. </para>
+ 	            /// </summary>
+ 	            /// <param name="ID" type="String">The requested item ID.</param>
+ 	            /// <returns type="ITerraExplorerObject" />
+ 	        },
+ 	        GetVisibility: function (ID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        IsGroup           : function(ID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        IsLayer	          : function() {
+ 	            /// <summary>Obsolete. To determine whether a Project Tree element is a feature layer, use IProjectTree.GetObject to return an interface to the element and then ITerraExplorerObject. ObjectType to get the element type.</summary>
+ 	            /// <deprecated type="remove"></deprecated>
+            },
+ 	        IsLocked          : function(ID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        LoadFlyLayer: function (FlyURL,ParentGroupID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        LockGroup: function (GroupID,bLock) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        RenameGroup: function (GroupID,GroupName) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        SaveAsFly: function (FlyName,GroupID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        SaveAsKml: function (KmlName,GroupID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        SelectItem: function (ID,Flags,Reserved) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        SetActivationCode: function (GroupID,ACode,ActivationParam) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        SetClientData: function (ID,Namespace,ClientData) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        SetGroupEndTime: function (GroupID,newVal) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        SetGroupLocation: function (GroupID,newVal) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        SetGroupStartTime: function (GroupID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        SetParent: function (ID,ParentGroupID) {
+ 	            /// <summary></summary>
+ 	            /// <returns type="" />
+ 	        },
+ 	        SetVisibility: function (ID,bShow) {
+ 	            /// <summary>This method sets a Project Tree item’s visibility on the terrain</summary>
+ 	            /// <returns type="Undefined" />
+ 	        },
+ 	        SortGroup: function (GroupID,SType) {
+ 	            /// <summary>Sorts the items in the group according to the specified sort type</summary>
+ 	            /// <returns type="Undefined" />
+            },
+
+            //$$$$$$$$$$$$
+            /*
+ 	            
+            
+            Creates a group in a “locked” (collapsed) mode. When locked, TerraExplorer Viewer users cannot expand the group to view its contents
+            Deletes an item from the Project Tree (and from the terrain, if that item was a terrain object)
+            Opens the property sheet to edit a specified item.
+            Opens the Multi Edit property sheet to edit multiple specified items.
+            Enables changes to be redrawn or prevents them from being redrawn in the Project Tree.
+            Terminates the edit item mode.
+            Expands the specified group so that it displays all of its children, or collapses it.
+            Finds an item using a path to that item.
+            Returns the activation code assigned for the specified group.
+            Returns the text string describing the group’s attribute data. This information is available for general use in your application. 
+            Returns the specified group’s end date and time.
+            Returns the location of the specified group.
+            Returns the specified group’s start date and time.
+            Returns the name of a specific item using its ID.
+            Returns an interface to the IFeatureLayer66 object based on the specified group ID.
+            Retrieves the Project Tree item that has the specified relationship, indicated by the Code parameter, to another item whose ID is specified.
+            Returns an ITerraExplorerObject interface to an object based on its ID. 
+            Returns the visibility status of a Project Tree item on the terrain. 
+            Determines whether or not a ProjectTree item is a group.
+            Determines whether or not  a group is locked.
+            Loads a Fly file into the current project. All the objects from the Fly file, including locations, routes and hyperlinks, are added. 
+            Sets a group to a “lock” (collapsed) status mode. When a group is locked, users of TerraExplorer Viewer cannot expand the group and view its contents.
+            Renames a group in the Project Tree.
+            Saves the specified group to a Fly file.
+            Saves the specified group to a KML file.
+            Selects an item in the Project Tree.
+            Determines the default activation action executed when clicking on the locked group symbol.
+            Stores a text string in the group global namespace.
+            Determines the date and time when the group should stop being visible
+            Determines the location parameters for a specified group.
+            Determines the date and time when the group should start being visible
+            Moves an item to a different group.
+            Sets a ProjectTree item to be visible, or not visible, on the terrain.
+ 	            ShowSubTree	Reserved
+            Sorts the items in the group according to specified sort type.
+
+            */
+
+            //$$$$$$$$$$$$
         }
     }
+    function ITerrain() {
+        return {};
+    }
+    function IVersionInfo() {
+        return {};
+    }
+    function IWindow() {
+        return {};
+    }
+
 
     function ITerraExplorerObject() {
         return {
